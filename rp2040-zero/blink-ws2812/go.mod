3606de77f3e8580e62f5e934ebca24b5ac883ca9
Leon Matthews <leon.matthews@dbvisit.com>
2025-05-15 15:18:14 +1200

    Minimal colour changing example for WS2812 LEDs

---
+++ rp2040-zero/blink-ws2812/go.mod
@@ -0,0 +1,5 @@
+module blink-ws2812
+
+go 1.24.2
+
+require tinygo.org/x/drivers v0.31.0
--- rp2040-zero/blink-ws2812/main.go
+++ rp2040-zero/blink-ws2812/main.go
@@ -1,1 +1,29 @@
 // Blink the onboard WS2812 LED
+package main
+
+import (
+    "image/color"
+    "machine"
+    "time"
+
+    "tinygo.org/x/drivers/ws2812"
+)
+
+var (
+    red = color.RGBA{R: 0xff}
+    blue = color.RGBA{B: 0xff}
+    green = color.RGBA{G: 0xff}
+    colors = []color.RGBA{red, blue, green}
+)
+
+func main() {
+    var neo machine.Pin = machine.NEOPIXEL
+    neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
+    ws := ws2812.NewWS2812(neo)
+    for {
+        for i := range colors {
+            ws.WriteColors(colors[i:i+1])
+            time.Sleep(1 * time.Second)
+        }
+    }
+}
